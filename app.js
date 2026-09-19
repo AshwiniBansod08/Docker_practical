ba4dd0a697601bc73936fc7e36887d8013632efe
Ashwini Bansod <khawaskarashwini@gmail.com>
2026-09-19 11:16:51 +0530

    nodejs file added

---
+++ app.js
@@ -0,0 +1,5 @@
+console.log("=================================");
+console.log("   Welcome to Node.js Docker!");
+console.log("   Container is running successfully!");
+console.log("   Hello Ashwini 👋");
+console.log("=================================");
